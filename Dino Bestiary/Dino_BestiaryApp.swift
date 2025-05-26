340a16d4135fa43a3350c9766022cf8d06988ad0
Renan Maganha <maganha.renan@gmail.com>
2025-05-26 18:31:03 -0300

    Initial Commit

---
+++ Dino Bestiary/Dino_BestiaryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dino_BestiaryApp.swift
+//  Dino Bestiary
+//
+//  Created by Renan Maganha on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct Dino_BestiaryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Dino BestiaryTests/Dino_BestiaryTests.swift
@@ -0,0 +1,17 @@
+//
+//  Dino_BestiaryTests.swift
+//  Dino BestiaryTests
+//
+//  Created by Renan Maganha on 26/05/25.
+//
+
+import Testing
+@testable import Dino_Bestiary
+
+struct Dino_BestiaryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
